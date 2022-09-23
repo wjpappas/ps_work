a70daa8694f59553d02ebe643ee8bf30bf316e1a
wjpappas <wjpappas@gmail.com>
2022-09-23 16:47:56 -0700

    refactor

--- bump.py
+++ bump.py
@@ -8,16 +8,31 @@ import requests
 import urllib.parse
 
 '''
-mempfile = sys.argv[1]          #emloyee job file
-sempfile = sys.argv2]           #employee driver file
-empadd = sys.argv[3]            #emloyee address file
-custjob = sys.argv[4]           #customer job file
+#read all files(4) --> list
+# get employee from employee job list-file
+# get employee from employee driver list-file
+# make list, intersection of employees
+# make list, employee-address dict
+# get jobs worked from employee job list
+#
+emp_job_file = sys.argv[1]          #emloyee job file
+emp_dr_file = sys.argv2]           #employee driver file
+emp_add_file = sys.argv[3]            #emloyee address file
+cust_job_file = sys.argv[4]           #customer job file
 outname = sys.argv[5]           #employee miles output file
+
+
+
+
+
+
+
+
 '''
-mempfile = 'test04.csv'       # emloyee job file
-sempfile = 'tested.csv'     # employee driver file
-empadd = 'emp.csv'          # emloyee address file
-custjob = 'ccl.csv'         # customer job file
+emp_job_file = 'test04.csv'       # emloyee job file
+emp_dr_file = 'tested.csv'     # employee driver file
+emp_add_file = 'emp.csv'          # emloyee address file
+cust_job_file = 'ccl.csv'         # customer job file
 
 outname = 'emp_miles.csv'
 outputFile = open(outname, 'w')
@@ -27,7 +42,7 @@ api_key = 'AIzaSyC5KTP5tuu4I-g5amjmO3eLpTOZKeur8KQ'
 
 dist_keyRegex = re.compile(r'\s*\"text\" : \"(\d{1,4}.\d) mi\"')
 job_keyRegex = re.compile(r':([\d]{2}[-][\d]{3})')
-employeeRegex = re.compile(r'(Total )(\w+\s*\w*,\s\w+\s*\w*)')
+empRegex = re.compile(r'(Total )(\w+\s*\w*,\s\w+\s*\w*)')
 job_dayRegex = re.compile(r'([1-9]\d*.\d{2})')
 
 def listFile(infile):
@@ -36,46 +51,36 @@ def listFile(infile):
         array = list(file_read)
     return array
 
-def makeList(array,regex,gid):
+def makeList(array, regex, gid):
     mylist = []
     for liste in array:
-        if (regex.search(liste[0])):
-            gggmz = regex.search(liste[0])
+        gggmz = regex.search(liste[0])
+        if gggmz:
             listx = gggmz.group(gid)
             if listx not in mylist:
                 mylist.append(gggmz.group(gid))
     return mylist
 
-def getDays(mempvar, sparray, employeeRegex, job_keyRegex, job_dayRegex):
-    x = 0
-    minidict = {}
-    while x < len(sparray):
-        if mempvar in sparray[x]:
-            isJob = True
-            while isJob:
-                x = x+1
-                if (employeeRegex.search(sparray[x][0])):
-                    isJob = False
-                if (job_keyRegex.search(sparray[x][0])):
-                    ggllz = job_keyRegex.search(sparray[x][0])
-                    job_key = ggllz.group(1)
-                    z = 1
-                    daycount = 0
-                    while z < len(sparray[x])-1:
-                        if (job_dayRegex.search(sparray[x][z])):
-                            daycount += 1
-                        z = z+1
-                    minidict[job_key] = daycount
-        x = x+1
-    return minidict
-
-def makeBiglist(bvar, array,mydict):
-    dictlst = {}
-    for bg in array:
-        if bvar in bg:
-            dictlst = {'emp': bvar, 'addr': bg[1], 'job': mydict}
-            break
-    return dictlst
+def getDays(emp_add_list, emp_job, empRegex, job_keyRegex, job_dayRegex):
+    big_list = []
+    mydict = {}
+    for e_val in emp_add_list:
+        empvar = e_val[0]
+        isJob = False
+        for job_var in emp_job:
+            if isJob:
+                ggllz = job_keyRegex.search(job_var[0])
+                job_key = ggllz.group(1)
+                daycount = 0
+                for xday in job_var:
+                    if job_dayRegex.search(xday):
+                        daycount += 1
+                mydict[job_key] = daycount-1
+            if empvar in job_var:
+                isJob = True
+            if ("Total " + empvar) in job_var:
+                isJob = False   # or maybe "Break"
+        big_list.append({'emp': empvar, 'addr': e_val[1], 'job': mydict})
 
 def makeJoblist(jobvar, jbarray):
     job = {}
@@ -85,35 +90,20 @@ def makeJoblist(jobvar, jbarray):
             break
     return job
 
-biglist = []
-joblistx = []
+emp_job = listFile(emp_job_file)
+emp_dr = listFile(emp_dr_file)
+emp_add = listFile(emp_add_file)
+cust_job = listFile(cust_job_file)
 
-sparray = listFile(mempfile)
-
-memp1 = makeList(sparray, employeeRegex, 2)
-joblist = makeList(sparray, job_keyRegex, 1)
-
-array = listFile(empadd)
-jbarray = listFile(custjob)
-
-print(joblist)
-jaddlist = [makeJoblist(jobvar, jbarray) for jobvar in joblist]
-
-f = open('tested.csv')
-lines = list(f)
-f.close
-
-new_lst = [x[1:-2] for x in lines]
-memp2 = []
-for x in new_lst:
-    for list in memp1:
-        if x in list:
-            memp2.append(x)
+emp_a = makeList(emp_job, empRegex, 2)
+emp_ab = [set(emp_job).difference(set(emp_dr))]
+emp_abc = [abc for ab, abc in zip(emp_ab, emp_add) if ab == abc[0]]
+job_list = makeList(emp_job, job_keyRegex, 1)
+print(job_list)
+job_add_d = [makeJoblist(jobvar, cust_job) for jobvar in job_list]
 
 # generate data structure per employee
-for mempvar in memp2:
-    mydict = getDays(mempvar, sparray, employeeRegex, job_keyRegex, job_dayRegex)
-    biglist.append(makeBiglist(mempvar, array, mydict))
+biglist = getDays(emp_abc, emp_job, empRegex, job_keyRegex, job_dayRegex)
 # print(biglist)
 
 hurl = "https://maps.googleapis.com/maps/api/directions/json?"
@@ -123,12 +113,12 @@ for glist in biglist:
 
     originx = glist.get('addr')
     for addkey in glist['job'].keys():
-        dest = next(x for x in jaddlist if x["job"] == addkey)['addr']
+        dest = next(x for x in job_add_d if x["job"] == addkey)['addr']
         origin = urllib.parse.quote(originx)
         destination = urllib.parse.quote(dest)
         url = hurl + 'origin=' + origin + '&destination=' + destination + '&key=' + api_key
 
-        payload={}
+        payload = {}
         headers = {}
 
         response = requests.request("GET", url, headers=headers, data=payload)
