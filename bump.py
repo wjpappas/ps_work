5066f91e70d1ca1e16ccaf1ce9fa84a26a623d1d
wjpappas <wjpappas@gmail.com>
2023-01-12 14:55:25 -0800

    95% done?

--- bump.py
+++ bump.py
@@ -3,15 +3,14 @@
 import re
 import csv
 import logging
+from datetime import datetime
 from configparser import ConfigParser
 import argparse
 import textwrap
-# import sys
 import requests
-# import json
 import urllib.parse
 
-logging.basicConfig(filename='log_filename.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='log_filename.txt', level=logging.DEBUG, format='%(lineno)d - %(funcName)s - %(levelname)s - %(message)s')
 hurl = "https://maps.googleapis.com/maps/api/directions/json?"
 
 job_keyRegex = re.compile(r'([\d]{2}[-][\d]{3})')
@@ -28,9 +27,20 @@ outname = 'emp_miles.csv'
 # get jobs worked from employee job list
 #
 '''
+def _get_values():
+    """ Fetch the Paintsmith config values your configuration file.
+
+    Expects a configuration file named "secrets.ini" with structure:
+
+        [paintsmith]
+        overhead=22-000 OH
+    """
+    config = ConfigParser()
+    config.read("secrets.ini")
+    return config["paintsmith"]["overhead"]
 
 def _get_api_key():
-    """ Fetch the API key form your configuration file.
+    """ Fetch the API key from your configuration file.
 
     Expects a configuration file named "secrets.ini" with structure:
 
@@ -80,6 +90,23 @@ def get_distkey(hurl, origin, dest, api_key):
             break
     return dist_key
 
+def get_week_num(infile, *args):
+    """ Generate a weeknum tuple for day/job entry"""
+    wk_num = []
+    with open(infile) as in_file:
+        line_read = next(csv.reader(in_file))
+    line_read.pop()
+    idx0 = (datetime.strptime(line_read[1], "%b %d, %y")).isocalendar().weekday
+    lastday = (datetime.strptime(line_read[-1], "%b %d, %y")).day
+    for dstring in line_read:
+        if dstring != '':
+            wkx = (datetime.strptime(dstring, "%b %d, %y")).isocalendar().week
+            if wkx not in wk_num:
+                wk_num.append(wkx)
+    idxN = (datetime.strptime(dstring, "%b %d, %y")).isocalendar().weekday
+    logging.debug("%s %s %s %s", wk_num, idx0, idxN, lastday)
+    return wk_num + [idx0-7, idxN, lastday]
+
 def listFile(infile):
     """Read file into list."""
     with open(infile) as in_file:
@@ -98,9 +125,9 @@ def makeList(array, regex, gid):
                 mylist.append(gggmz.group(gid))
     return mylist
 
-def getDays(emp_add, emp_job, empRegex, jobRegex):
+def getDays(emp_add, emp_job, wk_num, empRegex, jobRegex):
     """Build the employee, address, job no. dictionary."""
-    job_dayRegex = re.compile(r'([1-9]\d*.\d{2})')
+
     big_list = []
     for e_val in emp_add:                       # select emp/address element
         mydict = {}
@@ -112,14 +139,31 @@ def getDays(emp_add, emp_job, empRegex, jobRegex):
             if isJob:
                 ggllz = jobRegex.search(job_var[0])
                 job_key = ggllz.group(1)
-#                daycount = len(list(filter(job_dayRegex.match, job_var)))
-                daycount = len([x for x in job_var if job_dayRegex.match(x)])
-                mydict[job_key] = daycount-1
+                daycount = get_daycount(job_var, wk_num)
+                mydict[job_key] = daycount
             if empvar in job_var:
                 isJob = True
         big_list.append({'emp': empvar, 'addr': e_val[1], 'job': mydict})
         logging.debug(big_list[-1])
     return big_list
+
+def get_daycount(hours, weeks):
+    """Break out hours for each week"""
+    hours.pop()
+    first, last, dayN = weeks[-3], weeks[-2], weeks[-1]
+    inc = 7
+    start = first + inc
+    stop = dayN+inc
+    st = 1
+    wk_list = []
+    for i in range(start, stop, inc):
+        sp = i
+        if i > dayN:
+            sp = sp + last - inc
+        wk_count = len([x for x in hours[st:sp] if float(x) > 0])
+        wk_list.append(wk_count)
+        st = sp
+    return wk_list
 
 def makeJoblist(jobvar, jbarray):
     """Attach an address to the active job."""
@@ -130,20 +174,16 @@ def makeJoblist(jobvar, jbarray):
             break
     return job
 
-'''
-emp_job_file = 'test04.csv'       # emloyee job file
-emp_dr_file = 'tested.csv'     # employee driver file
-emp_add_file = 'emp.csv'          # emloyee address file
-cust_job_file = 'ccl.csv'         # customer job file
-'''
 if __name__ == "__main__":
-#    bob = read_user_cli_args()
-#    print(bob.file[0], bob.file[1], bob.file[2], bob.file[3])
-     input_list = read_user_cli_args()
-logging.debug(input_list.file)
+    oh_code = _get_values()
+    input_list = read_user_cli_args()
+    empjobfile = (input_list.file[0])
+    week_number = get_week_num(empjobfile)
+    logging.debug("x %s %s %s", input_list.file[0], week_number, oh_code)
 
 # try input list True
-emp_job, emp_dr, emp_add, cust_job = [listFile(x) for x in input_list.file]
+emp_jobx, emp_dr, emp_add, cust_job = [listFile(x) for x in input_list.file]
+emp_job = [x for x in emp_jobx if x[0] != oh_code]
 
 emp_a = makeList(emp_job, emp_Regex, 2)                 # select employees
 emp_ab = [ab[0] for ab in emp_dr if ab[0] in emp_a]     # driver from employees
@@ -155,13 +195,14 @@ logging.debug(job_list)
 job_add_d = [makeJoblist(jobvar, cust_job) for jobvar in job_list]
 
 # generate data structure, employee, address, jobs{Job#:days,...}
-biglist = getDays(emp_abc, emp_job, emp_Regex, job_keyRegex)
-# print(biglist)
+biglist = getDays(emp_abc, emp_job, week_number, emp_Regex, job_keyRegex)
+print(biglist)
+print(week_number)
 
 """ For one employee record,                                  
     step though emp-jobs finding the distance to the cust-job."""
 
-outputFile = open(outname, 'w')
+outputFile = open(outname, 'w', newline='', encoding='utf-8')
 outputWriter = csv.writer(outputFile)
 api_key = _get_api_key()
 logging.debug(api_key)
@@ -179,20 +220,19 @@ for glist in biglist:
 
         dict_mile.update({addkey: dist_key})
     glist.update({'miles': dict_mile})
-    outputWriter.writerow([glist['emp']])
+    outputWriter.writerow(['\n'+glist['emp']])
 #    print(index(glist['emp']))
     outputWriter.writerow(['Job Num', 'Miles', 'Days', 'total'])
     v1 = glist['job']
     Xtotal = 0
     for k2 in v1:
-        days = (glist['job'][k2])
-        oneway = (glist['miles'][k2])
         jobNum = str(k2)
-        total = int(oneway)*int(days)*2
+        oneway = (glist['miles'][k2])
+        days = (glist['job'][k2])
+        total = int(oneway)*int(sum(days))*2
         Xtotal += total
         outputWriter.writerow([jobNum, oneway, days, total])
-    outputWriter.writerow([glist['emp']+' Total miles=', '', '', '', str(Xtotal)])
+    outputWriter.writerow([glist['emp']+' Total miles='+str(Xtotal)])
     print(glist['emp']+', Total miles='+str(Xtotal))
 
 outputFile.close()
-
